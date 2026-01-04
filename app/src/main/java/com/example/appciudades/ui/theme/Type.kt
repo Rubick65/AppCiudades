8ea147911b36907cd1fb25fee389660bfa5bfb5b
Hugo de Pablo <hugdpalo@gmail.com>
2026-01-04 18:22:45 +0100

    Fuentes

--- app/src/main/java/com/example/appciudades/ui/theme/Type.kt
+++ app/src/main/java/com/example/appciudades/ui/theme/Type.kt
@@ -2,11 +2,37 @@ package com.example.appciudades.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.appciudades.R
 
 // Set of Material typography styles to start with
+
+
+val Oswald = FontFamily(
+    Font(R.font.oswald_bold),
+    Font(R.font.oswald_regular)
+)
+
+val Bbhbogle = FontFamily(
+    Font(R.font.bbhbogleregular)
+)
+
+val Bebasneue = FontFamily(
+    Font(R.font.bebasneue_regular)
+)
+
+val PlayFairDisplau = FontFamily(
+    Font(R.font.playfairdisplay_bold),
+    Font(R.font.playfairdisplay_regular)
+)
+
+val Lobster = FontFamily(
+    Font(R.font.lobster_regular)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,10 +40,10 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    /*Para el titulo
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Oswald,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
@@ -30,5 +56,5 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+     */
 )
